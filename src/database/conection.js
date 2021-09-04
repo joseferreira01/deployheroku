d4743b4e41c30747c6ef1d1d65e244988c0609a2
josé <joseifpb2015@gmail.com>
2021-09-04 10:19:18 -0300

    alt st

--- src/database/conection.js
+++ src/database/conection.js
@@ -1,5 +1,5 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
-const conection = knex(configuration.development);
+const conection = knex(configuration.staging);
 
 module.exports = conection;
